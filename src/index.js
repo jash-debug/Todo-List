baa4c098a5358d475997b161c16b0442e1716e4e
jash-debug <jash-debug@github.com>
2022-11-12 17:27:34 +0300

    add clear button functionality to remove completed lists

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const tasksList = new Tasks();
 
 const newTaskInput = document.querySelector('.enter-task');
 const tasksUL = document.querySelector('.tasks-list');
+const clearBtn = document.querySelector('.clear-btn>button');
 
 window.addEventListener('load', () => {
   if (localStorage.getItem('tasks')) {
@@ -30,4 +31,13 @@ newTaskInput.addEventListener('keypress', (e) => {
       .createTaskElement(tasksList.tasksList[tasksList.tasksList.length - 1]);
     tasksUL.append(newTask);
   }
+});
+
+clearBtn.addEventListener('click', () => {
+  tasksList.tasksList = tasksList.tasksList.filter((item) => item.completed === false);
+  for (let i = 0; i < tasksList.tasksList.length; i += 1) {
+    tasksList.tasksList[i].index = i;
+  }
+  localStorage.setItem('tasks', JSON.stringify(tasksList.tasksList));
+  tasksList.updateDisplay();
 });
