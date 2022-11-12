8c33f4c30475c0cae116323bec3f40b24074c700
jash-debug <jash-debug@github.com>
2022-11-12 07:36:11 +0300

    add javascript functionality to create list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const tasksList = document.querySelector('.tasks-list');
+const tasks = [
+  {
+    description: 'Learn Javascript',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Go Play',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Run Marathon',
+    completed: false,
+    index: 3,
+  },
+];
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+function populateTasksSection(tasks) {
+  tasks.forEach((task) => {
+    const item = document.createElement('li');
+    item.classList.add('list-item');
+    item.innerHTML = `<button><i class="fa-regular fa-square"></i></button>
+        <span>${task.description}</span>
+        <i class="fa-solid fa-ellipsis-vertical"></i>`;
+    tasksList.append(item);
+  });
 }
 
-document.body.appendChild(component());
+populateTasksSection(tasks);
